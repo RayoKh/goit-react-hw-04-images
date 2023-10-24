aede682e746ea4a8a6cafe49404f800ea41191ea
RayoKh <platypus1703@gmail.com>
2023-10-24 19:36:26 +0300

    Update api.jsx

--- src/components/api/api.jsx
+++ src/components/api/api.jsx
@@ -1,16 +1,29 @@
-const BASE_URL = `https://pixabay.com/api/`;
+import axios from 'axios';
+
+const BASE_URL = 'https://pixabay.com/api/';
 const API_KEY = '38556698-b3856d862faf8b4ab27b44b8c';
 
 const fetchImages = async (query, page, perPage) => {
-  const response = await fetch(
-    `${BASE_URL}?key=${API_KEY}&image_type=photo&orientation=horizontal&q=${query}&page=${page}&image_type=photo&orientation=horizontal&per_page=${perPage}`
-  );
+  try {
+    const response = await axios.get(BASE_URL, {
+      params: {
+        key: API_KEY,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        q: query,
+        page,
+        per_page: perPage,
+      },
+    });
 
-  if (response.ok) {
-    return response.json();
+    if (response.data.hits.length === 0) {
+      return Promise.reject(new Error(`Oops... that's all`));
+    }
+
+    return response.data;
+  } catch (error) {
+    return Promise.reject(error);
   }
-
-  return await Promise.reject(new Error(`Oops... that's all `));
 };
 
 const api = {
